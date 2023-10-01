fe029ffca41fccbcccac23f45bc47bf623819e3b
Gani-23 <saiganesh7989@gmail.com>
2023-10-01 12:15:07 +0530

    first commit

---
+++ src/Components/Extrass.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import './Extrass.css';
+
+const Kongo = () => {
+  return (
+    <React.Fragment>
+      <div className="Newspaper">
+        <h1 className="header">Research with me.</h1>
+        <div className="subhead">
+          Python | Java | C# | .NET | AWS | XAML | XAMARIN | React JS | OOPS (Object Oriented language) 
+        </div>
+
+        <div className="content">
+          <div className="colos">
+
+            <div className="colo">
+       <h1>What do yo need</h1>
+       <p className="para">
+       In the grand tapestry of human existence, it is our unique capacity for setting clear objectives that sets us apart. This ability allows us to chart our course, to navigate the complexities of life with purpose and intentionality. A clear goal serves as a beacon, guiding our efforts and energies towards a specific destination.
+
+Coupled with this, our innate passion for learning fuels the engine of progress. It's this burning desire to acquire new knowledge, to uncover hidden truths and explore uncharted territories, that propels us forward. This passion is the catalyst for growth, the driving force behind our evolution as individuals and as a collective society.
+
+Yet, the true magic happens when we bridge the gap between theory and practice. It's in the application of our learning that the real transformation occurs. We absorb, internalize, and then bring forth our newfound wisdom into the tangible world. Through action, we breathe life into our aspirations, turning abstract concepts into concrete realities.
+
+Adaptation becomes the cornerstone of this process. It is our ability to flex and mold ourselves to the demands of a dynamic world that truly sets us on the path of progress. We refine our approaches, refine our understanding, and refine ourselves in the crucible of experience.
+
+And let us not forget, this journey is not for mere self-aggrandizement, but for the greater good. It is through our collective efforts that we build a society that thrives, that advances, and that leaves a legacy of positive change. Each instance of applied knowledge becomes a stepping stone towards a world that is more enlightened, more compassionate, and more harmonious.
+
+So, in celebrating this beautiful dance of goal-setting, learning, and application, we are celebrating the very essence of our humanity. It is a testament to our potential, our capacity to shape our destinies, and our responsibility to leave a lasting mark on this world. Keep embracing this process, for it is through it that we forge a brighter, more promising future for all.
+       </p>
+            </div>
+            <div className="colo">
+<h1>What drives you ?</h1>
+
+<p className="para">
+The spontaneous drive that propels us forward in life often stems from within ourselves. It's a powerful force, uniquely crafted by our dreams, desires, and aspirations. To truly harness this inner energy, it's crucial to become the person we've always envisioned ourselves to be. However, amidst the hustle and bustle of life, it's equally important to remember that we must occasionally take a step back and recharge.
+
+One fundamental truth is that the more we cling to someone, the more they might yearn for their own space and independence. This dynamic can be especially toxic when it comes to relationships. It's essential to recognize when certain people in our lives are not contributing positively to our well-being or personal growth. In such cases, it's liberating to let go and release ourselves from the grip of toxic influences.
+
+By doing so, we free up valuable mental and emotional space, allowing us to focus on nurturing our true selves. This process of self-discovery and personal growth can be incredibly rewarding. We can explore our passions, develop new skills, and pursue our goals with unwavering determination.
+
+Becoming the person we've always wanted to be requires both self-reflection and action. It means shedding the layers of self-doubt, societal expectations, and negative influences that may have held us back. It means embracing our authentic selves and embracing the journey of self-improvement.
+
+So, in the pursuit of your dreams and personal growth, remember that the spontaneous drive within you is a powerful catalyst for change. Let go of toxic people and circumstances that hinder your progress, and instead, channel your energy into becoming the best version of yourself. Take those necessary breaks to recharge, but always keep moving forward towards the person you aspire to be.
+       </p>
+
+          </div>
+          </div>
+          
+
+          
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Kongo;
